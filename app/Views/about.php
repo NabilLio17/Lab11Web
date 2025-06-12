bea445ee9351cbb2fa6eafde35d2e5020993d581
NabiLio17 <nabilsibasuki@gmail.com>
2025-06-12 16:51:11 +0700

    first commit

---
+++ app/Views/about.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+    <?= $this->include('template/header'); ?>
+
+    <h1>pemilikportal berita ini menyembunyikan indentitasnya</h1><?= $this->include('template/footer'); ?>
+</body>
+
+</html>
